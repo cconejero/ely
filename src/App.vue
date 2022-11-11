c08ce60dd151833585ddda37403436dc7467c438
Claudio Conejero <conejero.claudio@gmail.com>
2022-11-11 14:43:23 -0300

    Countdown y estilos

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-
+import {Countdown} from 'vue3-flip-countdown';
 </script>
 
 <template>
   <div>
-
+    <h1 class="mb-6">Moncho llega en:</h1>
+    <Countdown deadline="2022-11-19 16:55:00" />
   </div>
 </template>
