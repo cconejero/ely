f4c456985b15a13f966d8c5adcdfdeb9a4852279
Claudio Conejero <conejero.claudio@gmail.com>
2022-11-11 14:59:17 -0300

    PWA

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,12 @@ import {Countdown} from 'vue3-flip-countdown';
 
 <template>
   <div>
-    <h1 class="mb-6">Moncho llega en:</h1>
-    <Countdown deadline="2022-11-19 16:55:00" />
+    <h1 class="mb-6 text-gray-300">Moncho llega en:</h1>
+    <Countdown deadline="2022-11-19 16:55:00"
+               :labels="{days: 'Días',hours: 'Horas',minutes: 'Minutos',seconds: 'Segundos',}"
+               mainColor="#aaaaaa"
+               secondFlipColor="#999999"
+               labelColor="#aaaaaa"
+    />
   </div>
 </template>
