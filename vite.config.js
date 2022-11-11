3e17d913c00387a37424836698b3be1067485329
Claudio Conejero <conejero.claudio@gmail.com>
2022-11-11 15:57:03 -0300

    Assets nuevos

--- vite.config.js
+++ vite.config.js
@@ -8,27 +8,33 @@ export default defineConfig({
       vue(),
       VitePWA({
           registerType: 'autoUpdate',
-          injectRegister: 'auto',
-          name: 'Moncho llega en...',
-          short_name: 'Moncho',
-          icons: [
-              {
-                  src: 'src/assets/android-chrome-192x192.png',
-                  sizes: '192x192',
-                  type: 'image/png'
-              },
-              {
-                  src: 'src/assets/android-chrome-512x512.png',
-                  sizes: '512x512',
-                  type: 'image/png'
-              },
-              {
-                  src: 'src/assets/android-chrome-512x512.png',
-                  sizes: '512x512',
-                  type: 'image/png',
-                  purpose: 'any maskable'
-              }]
-
+          workbox: {
+              globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+          },
+          includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'android-chrome-192x192.png', 'android-chrome-512x512.png'],
+          manifest: {
+              name: 'Moncho llega en...',
+              short_name: 'Moncho',
+              description: 'Cuando llega Moncho',
+              theme_color: '#ffffff',
+              icons: [
+                  {
+                      src: './src/assets/android-chrome-192x192.png',
+                      sizes: '192x192',
+                      type: 'image/png'
+                  },
+                  {
+                      src: './src/assets/android-chrome-512x512.png',
+                      sizes: '512x512',
+                      type: 'image/png'
+                  },
+                  {
+                      src: './src/assets/android-chrome-512x512.png',
+                      sizes: '512x512',
+                      type: 'image/png',
+                      purpose: 'any maskable'
+                  }]
+          }
       }),
   ]
 })
