ff47da5f06e80d2976fd44ce118b44852a9121aa
Claudio Conejero <cconejero@octopus.com.ar>
2023-04-21 02:03:54 -0300

    a

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import Countdown from 'vue3-flip-countdown'
-import VuePwaInstallPlugin from "vue-pwa-install";
 
-createApp(App).use(Countdown).use(VuePwaInstallPlugin).mount('#app')
+createApp(App).use(Countdown).mount('#app')
